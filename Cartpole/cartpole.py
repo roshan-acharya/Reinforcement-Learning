1aab11b04aa7f25396540a5bdeab50ed55d6f765
Roshan Acharya <roshanacharya9808@gmail.com>
2025-12-18 22:33:11 +0545

    Created neural network

--- Cartpole/cartpole.py
+++ Cartpole/cartpole.py
@@ -1,13 +1,58 @@
 import gymnasium as gym
+import cv2
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import matplotlib.pyplot as plt
 
-env = gym.make("CartPole-v1", render_mode="human")
-state, info = env.reset()
-print(state)
-print(info)
-print(env.action_space)
-action=1
-state, reward, terminated, truncated, info = env.step(action)
-print(state)
-print(reward)
-print(terminated)
-print(info)
+episode_reward=[]
+
+class PolicyNetwork(nn.Module):
+    def __init__(self):
+        super(PolicyNetwork,self).__init__()
+        self.fc=nn.Sequential(
+            nn.Linear(4,128),
+            nn.ReLU(),
+            nn.Linear(128,2),
+            nn.Softmax(dim=-1)
+        )
+    def forward(self,x):
+        return self.fc(x)
+    
+#initialize env and hyperparameters
+env=gym.make("CartPole-v1")
+episodes = 500
+learning_rate = 0.001
+gamma = 0.95
+epsilon = 1.0
+epsilon_min = 0.01
+epsilon_decay = 0.995
+
+#Network and optimizer
+
+q_network=PolicyNetwork()
+optimizer=optim.Adam(q_network.parameters(),lr=learning_rate)
+criterion = nn.MSELoss()
+
+#Training an agent
+
+episode_reward=[]
+for episode in range(episodes):
+    state, _ =env.reset()
+    state = torch.tensor(state, dtype=torch.float32).unsqueeze(0)
+    total_reward=0
+
+    for time in range(500):
+        if np.random.rand() < epsilon:
+            action = env.action_space.sample()
+        else:
+            with torch.no_grad():
+                q_values=q_network(state)
+            action = torch.argmax(q_values).item()
+    
+    #Take action in environment
+    next_state, reward,done,_,_= env.step(action)
+
+
+
